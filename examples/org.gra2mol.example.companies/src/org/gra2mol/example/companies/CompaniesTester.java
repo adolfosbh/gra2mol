44f4bea24c3d7a91566e7ff4329eff719cf46a2e
Adolfo SBH <adolfosbh@gmail.com>
2016-04-05 15:17:06 +0100

    - Improving the experiment java program. - Moving the topology related experiment to a properly named folder - Adding the models for the scalability experiment.

--- examples/org.gra2mol.example.companies/src/org/gra2mol/example/companies/CompaniesTester.java
+++ examples/org.gra2mol.example.companies/src/org/gra2mol/example/companies/CompaniesTester.java
@@ -7,52 +7,104 @@ import gts.modernization.launcher.Gra2MoLInterpreterLauncher;
 import gts.modernization.launcher.Gra2MoLProcessLauncher;
 
 public class CompaniesTester {
-		
-	static String basePath = "../Grammar2Model.examples.101companies/";
-	static String examplesPath = "./examples/";
-	public static void main(String[] args) {
 	
-		String modelName = args[0];
-		Gra2MoLEnricher enricher = new Gra2MoLEnricher(
-				"company",
-				new File(basePath + "gra2mol/grammars/Company.g"),
-				new File(basePath + "gra2mol/gen/Company.g"));
+	static enum Usage {
+		TOPOLOGY_EXPERIMENT,
+		SCALABILITY_EXPERIMENT,
+		INCORRECT
+	}
+	
+	static String BASE_PATH = "../Grammar2Model.examples.101companies/";
+	static String MODELS_PATH = "./examples/";
+	
+	private static int NUM_OF_MODELS = 7;
+	
+	public static void main(String[] args) throws Exception {
+	
+		Usage usage = checkArgs(args);
 		
+		if (usage == Usage.INCORRECT)  {
+			return;
+		} else {
+			if (Usage.SCALABILITY_EXPERIMENT == usage) {
+				NUM_OF_MODELS = 25;
+				MODELS_PATH = "./examples/scalability";
+			} else {
+				NUM_OF_MODELS = 7;
+				MODELS_PATH = "./examples/topology";
+			}
+		}
+				
 		// warmup
-		Gra2MoLInterpreterLauncher warmupInterpreter = new Gra2MoLInterpreterLauncher(
-				new File(basePath + "gra2mol/src/transformation.g2m"),
-				new File(basePath + "gra2mol/metamodels/Company.ecore"),
-				"company", 
-				new File(examplesPath + "model2.output.xmi"),
-				"Company", 
-				"company", 
-				new File(examplesPath + "model2.101"));
-		warmupInterpreter.setCaseSensitive(true);
-		Gra2MoLProcessLauncher processLauncher = new Gra2MoLProcessLauncher(enricher, warmupInterpreter);
-		processLauncher.setActivePhase1(false); // We only need to interpret the transformation
-		
+		System.out.println("* Running Warmup *");
 		for (int i = 0; i < 3000; i++) {
+			Gra2MoLProcessLauncher processLauncher = new Gra2MoLProcessLauncher(createEnricher(), createInterpreter("model2"));
+			processLauncher.setActivePhase1(false); // We only need to interpret the transformation
 			processLauncher.launch();
 		}
 		
+		printMemory();
+		
 		// Model file execution
-		Gra2MoLInterpreterLauncher interpreter = new Gra2MoLInterpreterLauncher(
-						new File(basePath + "gra2mol/src/transformation.g2m"),
-						new File(basePath + "gra2mol/metamodels/Company.ecore"),
-						"company", 
-						new File(examplesPath +  modelName + ".output.xmi"),
-						"Company", 
-						"company", 
-						new File(examplesPath +  modelName + ".101"));
-		interpreter.setCaseSensitive(true);
-		System.out.println("*** Executing model " + modelName + ".101");
-		processLauncher = new Gra2MoLProcessLauncher(enricher, interpreter);
-		processLauncher.setActivePhase1(false); // We only need to interpret the transformation
-		for (int i = 0; i < 10; i++) {
+		System.out.println("* Running Final Measurement *");
+		for (int i=1; i <= NUM_OF_MODELS ; i++) {
+			System.gc();
+			Thread.sleep(1000);
+			String modelName = "model"+i;
+			//System.out.println("*** Executing model " + modelName + ".101");
+			Gra2MoLProcessLauncher processLauncher = new Gra2MoLProcessLauncher(createEnricher(), createInterpreter(modelName));
+			processLauncher.setActivePhase1(false); // We only need to interpret the transformation
 			processLauncher.launch();
 		}
 	}
 	
+	private static Usage checkArgs(String[] args) {
+		
+		if (args == null || args.length != 1) {
+			System.out.println("Incorrect arguments. Please specify -topology for topology analysis related experiment, otherwise -scalability for the scalability related experiment");
+			return Usage.INCORRECT;
+		}
+		
+		return "-topology".equals(args[0]) ? Usage.TOPOLOGY_EXPERIMENT
+				: "-scalability".equals(args[0]) ? Usage.SCALABILITY_EXPERIMENT
+				: Usage.INCORRECT;
+	}
 	
+	private static Gra2MoLEnricher createEnricher() {
+		
+		return new Gra2MoLEnricher(
+				"company",
+				new File(BASE_PATH + "gra2mol/grammars/Company.g"),
+				new File(BASE_PATH + "gra2mol/gen/Company.g"));
+	}
 	
+	private static  Gra2MoLInterpreterLauncher createInterpreter(String modelName) {
+		
+		Gra2MoLInterpreterLauncher interpreter = new Gra2MoLInterpreterLauncher(
+				new File(BASE_PATH + "gra2mol/src/transformation.g2m"),
+				new File(BASE_PATH + "gra2mol/metamodels/Company.ecore"),
+				"company", 
+				new File(MODELS_PATH +  modelName + ".output.xmi"),
+				"Company", 
+				"company", 
+				new File(MODELS_PATH +  modelName + ".101"));
+		interpreter.setCaseSensitive(true);
+		return interpreter;
+	}
+	
+	private static void printMemory() {
+				
+		Runtime runtime = Runtime.getRuntime();
+		int mb = 1024*1024;
+		
+        System.out.println("Max Memory (Mb):" + runtime.maxMemory() / mb);
+        
+        System.out.println("Total Memory (Mb):" + runtime.totalMemory() / mb);
+        
+        System.out.println("Used Memory (Mb):"
+            + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+ 
+        System.out.println("Free Memory (Mb):"
+            + runtime.freeMemory() / mb);
+	}
 }
